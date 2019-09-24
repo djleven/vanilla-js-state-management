d5b239073e69b0bd1865ce71ddb812d125e1124d
djleven <info@e-leven.net>
2019-09-24 19:36:14 +0300

    add preload images for smoother experience

--- js/store/actions.js
+++ js/store/actions.js
@@ -1,4 +1,4 @@
-import { httpRequest, getScorePercentage } from '../helpers/misc.js'
+import { httpRequest, getScorePercentage, getImagesToPreload } from '../helpers/misc.js'
 
 export default {
     getQuestions(context) {
@@ -8,6 +8,7 @@ export default {
         })
             .then((data) => {
                 data = JSON.parse(data)
+                getImagesToPreload(data.questions, true)
                 context.commit('loadQuestions', data.questions)
                 context.commit('updateQuestion')
                 context.commit('loadQuizMeta', {
@@ -20,6 +21,7 @@ export default {
                 console.error('Augh, there was an error!', err)
             })
     },
+
     moveToNextQuestion(context) {
         const currentIndex = context.state.currentIndex
 
@@ -36,7 +38,33 @@ export default {
             }
         }
     },
-    getEndResult(context) {
+    /**
+     * Get request for the results objects
+     *
+     * This function can be called from two places in the app's lifecycle:
+     *
+     * 1-) During the app's sequential lifecycle, after the final question, this function would
+     * normally be called right before - and as a prerequisite to - the final app stage (which is the
+     * quiz score evaluation).
+     * In this case, the initEvaluation needed for launching the evaluation stage, will be true.
+     *
+     *
+     * 2-) Under normal circumstances however, it will be called earlier on the lifecycle, from
+     * getImagesToPreload():
+     * In this case, when getImagesToPreload finishes pre-loading the images for the questions,
+     * it then proceeds to load the results objects, so we can then finally also pre-load the
+     * results images, for a smoother user experience.
+     *
+     * In this case the initEvaluation will be false.
+     * Finally, when case no2 occurs before case no1, the latter will not be called because the results
+     * will already be present in the store.
+     *
+     * @param context
+     * @param initEvaluation boolean
+     *
+     * @returns {void}
+     */
+    getEndResult(context, initEvaluation = false) {
         httpRequest({
             method: 'GET',
             url: 'https://proto.io/en/jobs/candidate-questions/result.json'
@@ -44,7 +72,11 @@ export default {
             .then((data) => {
                 data = JSON.parse(data)
                 context.commit('loadEvaluationResults', data.results)
-                context.dispatch('initEvaluation')
+                if(initEvaluation) {
+                    context.dispatch('initEvaluation')
+                } else {
+                    getImagesToPreload(data.results)
+                }
             })
             .catch((err) => {
                 console.error('Augh, there was an error!', err)
