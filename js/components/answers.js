efd9811507e618164d3c152b8d0e4729c90e0c74
djleven <info@e-leven.net>
2019-09-28 14:19:07 +0300

    add ability to choose when to submit single select answer

--- js/components/answers.js
+++ js/components/answers.js
@@ -13,11 +13,11 @@ export default class Answers extends Component {
     }
 
     /**
-     * Loop the possible answers and generate a list of multiple selectable elements
+     * Loop the possible answers and generate a list of selectable elements
      *
      * @returns {void}
      */
-    generateMultipleSelectionElements () {
+    generateSelectionElements () {
         const question = store.state.currentQuestion
 
         question.possible_answers.forEach((q, index) => {
@@ -34,30 +34,13 @@ export default class Answers extends Component {
             this.element.appendChild(newlabel)
         })
     }
+
     /**
-     * Loop the possible answers and generate a list of single selectable elements
+     * Create a submit button for answering the question
      *
      * @returns {void}
      */
-    generateSingleSelectionElements () {
-        const question = store.state.currentQuestion
-
-        question.possible_answers.forEach((q, index) => {
-            let element = document.createElement('button');
-            element.onclick = answerEvents.evaluateAnswer
-            element.className ='pure-button'
-            element.innerHTML = question.possible_answers[index].caption
-            element.value = question.possible_answers[index].a_id
-            this.element.appendChild(element)
-        })
-    }
-
-    /**
-     * Create a submit button for multiple selection questions
-     *
-     * @returns {void}
-     */
-    createMultipleSelectionSubmitButton () {
+    createSelectionSubmitButton () {
         let button = document.createElement('button')
         let div = document.createElement("div")
         div.className ='submit'
@@ -88,13 +71,12 @@ export default class Answers extends Component {
      * @returns {void}
      */
     render() {
-        const question = store.state.currentQuestion
 
         if (!store.state.questions.length) {
             return
         }
 
-        if (!question.hasOwnProperty('possible_answers')) {
+        if (!store.state.currentQuestion.hasOwnProperty('possible_answers')) {
             console.log('Error: missing possible answers')
             return
         }
@@ -110,15 +92,10 @@ export default class Answers extends Component {
         // Don't show answer options after submission when displaying the answer
         if(!store.state.showCorrectAnswer) {
 
-            if (question.question_type === 'mutiplechoice-multiple') {
+            this.generateSelectionElements()
 
-                this.generateMultipleSelectionElements()
-                // Add a submit button since it's a multiple checkbox selection
-                this.createMultipleSelectionSubmitButton()
-
-            } else {
-                this.generateSingleSelectionElements()
-            }
+            // Add a submit button to move to next question
+            this.createSelectionSubmitButton()
         }
 
 
